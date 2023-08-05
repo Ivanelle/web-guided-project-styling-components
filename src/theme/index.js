3c7e94f7ad5c22cd585b3aa5827a133d1c36d10e
Ivanelle Hunt <ivanellep@gmail.com>
2023-08-04 17:29:03 -0700

    wip

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,1 +1,9 @@
 // theme object here
+export default {
+    primaryColor: 'royalblue',
+    secondaryColor: 'lightblue',
+    tertiaryColor: 'aquamarine',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: '(max-width: 550px)'
+}
